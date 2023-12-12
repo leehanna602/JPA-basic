91698cf030a0d5228646ec894662af4228901161
leehanna602 <2005haba98@gmail.com>
2023-12-13 00:56:16 +0900

    섹션7 고급매핑 - 2.Mapped Superclass-매핑 정보 상속

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -18,19 +19,15 @@ public class JpaMain {
 
         // code
         try {
-            Movie movie = new Movie();
-            movie.setDirector("aaaa");
-            movie.setActor("bbbb");
-            movie.setName("바람과함께 사라지다");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setName("user");
+            member.setCreatedBy("lee");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
